d520b78a3f94c5c3c61fb30b10a2ee38345b4852
Mikhail Pospelov <mixan946@gmail.com>
2015-10-26 14:04:02 +0300

    cleanup

--- pospelov/main.cpp
+++ pospelov/main.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <regex>
 #define READ_END 0
 #define WRITE_END 1
 
 using namespace std;
 const char EXAMPLE_USAGE[] = "Example usage of program: ./a.out file_name prototype";
-char AR_COMMAND[] = "/bin/ar";
 
 class ArFinder{
 
-  void print_prototype_matches(char *prototype){
-    // for (vector<string>::iterator it = archive_names.begin() ; it != archive_names.end(); ++it){
-    //   if(regex_match(*it, prototype_regex)){
-    //     cout << *it << endl;
-    //   }
-    // }
-  }
 public:
   ArFinder(char *file_name, char *prototype){
     char buff[1024];
-    char *command = strcat(AR_COMMAND, file_name);
     int pid;
     int fds[2];
-    regex prototype_regex = regex(prototype);
     pipe(fds);
 
     switch(pid = fork()){
